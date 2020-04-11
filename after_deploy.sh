e5bca758ab96d06608aa34e7c2e191bea67a7cba
Drishtu 11 <drishtu11@gmail.com>
2020-04-11 13:32:10 -0700

    Add corrdct heroku exec command after deploy

--- after_deploy.sh
+++ after_deploy.sh
@@ -4,4 +4,4 @@ set -e # Stop script from running if there are any errors
 #docker rm -f $(docker ps -a -q)
 #docker rmi -f $(docker images -q)
 #docker run topdownloads ./topdownloads 104.154.94.138 jcenter-cache .jar 2
-heroku run docker run topdownloads ./topdownloads 104.154.94.138 jcenter-cache .jar 2 -a topdownloads
+heroku run /app/bin/topdownloads 104.154.94.138 jcenter-cache .jar 2 -a topdownloads
