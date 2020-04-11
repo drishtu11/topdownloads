a23f0867c7cbc4c2eab0d68c3b30e2f37799fe39
Drishtu 11 <drishtu11@gmail.com>
2020-04-11 13:28:37 -0700

    Add Heroku exec command

--- after_deploy.sh
+++ after_deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e # Stop script from running if there are any errors
 
-docker rm -f $(docker ps -a -q)
-docker rmi -f $(docker images -q)
-docker run drishtu11/topdownloads ./topdownloads 104.154.94.138 jcenter-cache .jar 2
+#docker rm -f $(docker ps -a -q)
+#docker rmi -f $(docker images -q)
+#docker run topdownloads ./topdownloads 104.154.94.138 jcenter-cache .jar 2
+heroku run docker run topdownloads ./topdownloads 104.154.94.138 jcenter-cache .jar 2 -a topdownloads
